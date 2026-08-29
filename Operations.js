57f0f14c9b104343eded8907a54f6b1f6c02cf6e
[Thakur Vishal] <jmsthakurvishal2020@gmail.com>
2026-08-29 16:40:25 +0530

    Operators in js

---
+++ Operations.js
@@ -0,0 +1,34 @@
+// console.log(2+11);  // 13,
+// console.log(2-11);  // -9
+// console.log(2*11);  // 22
+// console.log(2/11);  // 0.181818181881
+// console.log(2%11);   // 2
+// console.log(2**11);  // 2048
+
+
+
+// console.log("2"+2);  // 22
+// console.log(2+"2");   // 22
+
+// console.log(2+2+"2");  // 4+"2" = "42"  left to right evaluation hoga 
+// // jab kabhi + ke sath string aayega concatenation behave karega ,, called as type coercion .
+
+// // console.log(2-"2");
+// // console.log(2-2-"2");
+
+// console.log("2"*"2");
+
+console.log(true);  // true
+console.log(+true); // 1 
+console.log(+"");  0
+// console.log(true+);  throws an error
+// console.log(""+);
+
+
+
+
+
+
+
+
+
