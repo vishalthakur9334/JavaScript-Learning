b21bb777d92d1be5dedd880a1de37807ea2c7bc0
[Thakur Vishal] <jmsthakurvishal2020@gmail.com>
2026-08-29 16:10:37 +0530

    DataType conversion in js

---
+++ Conversion.js
@@ -0,0 +1,38 @@
+// let a=23;
+// console.log(typeof a);
+
+// let b="23";
+// console.log(typeof b);
+
+// let c=Number(b);
+// console.log(c);
+// console.log(typeof c);
+
+// let p="23prsv";
+// console.log(typeof p);  // string 
+// let q=Number(p);   // conversion to number if possible 
+// console.log(q);    // NaN   not a number 
+
+// console.log(typeof q);   // number 
+
+
+// let score="23asd";  // true-1, null-0 ,  undefined- NaN  ,"23"-23 , 
+// let ans=Number(score);
+// console.log(typeof ans);  // number hi type aayega in any case 
+// console.log(ans);   // 1, 0, NaN,23 
+
+// Let's check now boolean value  
+
+
+
+// let eg=undefined;                // 1-true, 0-false, ""-false (empty string ),  "assd"-true , null-false, undefined-false
+// let ans=Boolean(eg);
+// console.log(ans);
+
+
+
+
+
+
+
+
